8e8c5198a73776fa35f30aa12dfa55d329e3f91c
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-04-21 16:12:41 +0200

    Implement contact box

--- views/single-project.blade.php
+++ views/single-project.blade.php
@@ -5,8 +5,8 @@
 <div class="container main-container">
     @include('partials.breadcrumbs')
 
-    <div class="grid  grid--columns">
-        <div class="grid-md-12 grid-lg-9">
+    <div class="grid grid--columns">
+        <div class="grid-md-12 grid-lg-8">
             @if (is_single() && is_active_sidebar('content-area-top'))
                 <div class="grid grid--columns sidebar-content-area sidebar-content-area-top">
                     <?php dynamic_sidebar('content-area-top'); ?>
@@ -16,7 +16,6 @@
             <div class="grid" id="readspeaker-read">
                 <div class="grid-sm-12">
                         {!! the_post() !!}
-
                         @include('partials.blog.type.post-single')
                 </div>
             </div>
@@ -49,6 +48,26 @@
                         </div>
                     </div>
                 @endif
+            @endif
+        </div>
+
+        <div class="grid-md-12 grid-lg-4">
+                        
+            {{-- Contacts --}}
+            @if ($project && $project['contacts'])
+                @php
+                    $contacts = get_post_meta(get_the_id(), 'contacts', false )[0];
+                @endphp
+
+                {{-- TODO: Translate labels --}}
+                <div class="box box-filled box-filled-1">
+                    <h4 class="box-title">Kontakt</h4>
+                    <div class="box-content">
+                        @foreach ($project['contacts'] as $contact)
+                            <p><b>Namn:</b> {{$contact['name']}}</br> <b>E-post: </b><a href="mailto:{{$contact['email']}}">{{$contact['email']}}</a></p>
+                        @endforeach
+                    </div>
+                </div>
             @endif
 
         </div>
