7c64ae61673ddbdea56b8b173cf05f2751452dce
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-04-21 11:39:00 +0200

    Add single project view

--- source/php/App.php
+++ source/php/App.php
@@ -10,6 +10,10 @@ class App
         new Import\Setup();
         new Options();
 
+
+        // Add view paths
+        add_filter('Municipio/blade/view_paths', array($this, 'addViewPaths'), 2, 1);
+
         // add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
         // add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
     }
@@ -30,5 +34,23 @@ class App
     public function enqueueScripts()
     {
         wp_register_script('project-manager-integration-js', PROJECTMANAGERINTEGRATION_URL . '/dist/' . \ProjectManagerIntegration\Helper\CacheBust::name('js/project-manager-integration.js'));
+    }
+
+    /**
+     * Add searchable blade template paths
+     * @param array  $array Template paths
+     * @return array        Modified template paths
+     */
+    public function addViewPaths($array)
+    {
+        // If child theme is active, insert plugin view path after child views path.
+        if (is_child_theme()) {
+            array_splice($array, 2, 0, array(PROJECTMANAGERINTEGRATION_VIEW_PATH));
+        } else {
+            // Add view path first in the list if child theme is not active.
+            array_unshift($array, PROJECTMANAGERINTEGRATION_VIEW_PATH);
+        }
+
+        return $array;
     }
 }
