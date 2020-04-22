a0d14fdd030316de94ed6b2d808fb2774745e2ea
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-04-22 15:05:25 +0200

    add public helper in_array_any

--- Public.php
+++ Public.php
@@ -1,3 +1,10 @@
 <?php
 
 // Public functions
+
+if (!function_exists('in_array_any')) {
+    function in_array_any($needles, $haystack)
+    {
+        return (bool)array_intersect($needles, $haystack);
+    }
+}
