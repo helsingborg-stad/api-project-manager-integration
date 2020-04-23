3c4dc6e267f3f28238fa585131d97076673095c0
zilaei <zzilaei@gmail.com>
2020-04-23 09:43:24 +0200

    Added map with pins to view

--- source/php/UI/Theme.php
+++ source/php/UI/Theme.php
@@ -12,30 +12,54 @@ class Theme
     
     public function __construct()
     {
-        // $this->VIEWS_PATHS = apply_filters('Municipio/blade/view_paths', array(
-        //     // get_stylesheet_directory() . '/views',
-        //     // get_template_directory() . '/views'
-            
-
-        // ));
-        
-        // $this->VIEWS_PATHS = array_unique($this->VIEWS_PATHS);
         $this->CACHE_PATH = WP_CONTENT_DIR . '/uploads/cache/blade-cache';
 
+        // TODO: Should not be running mapPlotData action during loop_start
         add_action('loop_start', array($this, 'mapPlotData'));
     }
 
     public function mapPlotData($query)
     {
-        // Add view
-        // if (isset($center) ) {
-        //     $blade = new Blade($this->VIEWS_PATHS, $this->CACHE_PATH);
-        //     echo $blade->view()->make('partials.area.map')->render();
-        // }
+        if (is_object($query) && $query->is_main_query() && isset($query->query['post_type'])) {
+            
+            if (is_single()) {
+                return false;
+            }
 
-        $result = null;
+            if(isset($_GET['filter'])) {
+                $allPosts = $query->posts;
+            } else {
+                 $allPosts = get_posts(array(
+                    'numberposts' => -1,
+                    'post_type' => $query->query['post_type']
+                ));
+            }
+
+            if (isset($allPosts) && is_array($allPosts) && !empty($allPosts)) {
+                
+                $result = array();
+
+                foreach ($allPosts as $postItem) {
+                    if ($postItem->post_status != "publish") {                    
+                        continue;
+                    }
+
+                    $map = get_field('map', $postItem->ID);
+                    if (!isset($map)) {
+                        continue;
+                    }
+
+                    $result[] = array(
+                        'location' => $postItem->post_title,
+                        'excerpt' => wp_trim_words($postItem->post_content, 20),
+                        'geo' => get_field('map', $postItem->ID)
+                    );
+                }
+            }
+        }
+
         $center = null;
-        
+
         $blade = new Blade(PROJECTMANAGERINTEGRATION_VIEW_PATH, $this->CACHE_PATH);
         echo $blade->view()->make('partials.area.map', array('data' => $result, 'center' => $center))->render();
     }
--- views/partials/area/map.blade.php
+++ views/partials/area/map.blade.php
@@ -1,9 +1,10 @@
-
+@if(isset($data) && is_array($data) && !empty($data))
+<div></div>
     <script>
-    //   var jsonPlots = {!!json_encode($data)!!}
+      var jsonPlots = {!!json_encode($data)!!}
     </script>
     <div class="grid-xs-12">
-
+    
       <style scoped>
         .area-map {
           width: 100%;
@@ -21,47 +22,47 @@
           <div id="areaMap"></div>
           <script>
             function areaInitMap() {
-
-              //Declare stuff
               var marker, item, map;
 
-              //Create new map
+              // Create new map
               map = new google.maps.Map(document.getElementById('areaMap'), {
                 zoom: 9,
                 // center: {!!json_encode($center)!!},
-                center: {lat: -34.397, lng: 150.644}
+
+                // Temporary center position, this shall be calculated
+                center: {lat: 56.050921, lng: 12.697870}
               });
 
-            //   for(var item in jsonPlots) {
+              for(var item in jsonPlots) {
 
-            //     //Get details about pin
-            //     name  = jsonPlots[item].location;
-            //     info  = jsonPlots[item].excerpt;
-            //     link  = jsonPlots[item].permalink;
+                //Get details about pin
+                name  = jsonPlots[item].location;
+                info  = jsonPlots[item].excerpt;
 
-            //     //Append html markup for infowindow
-            //     jsonPlots[item].info  = '<h3>' + name + '</h3>' + '<p>' + info + '</p>' + '<br><a target="_top" class="btn btn-md btn-primary" href="' + link + '"><?php _e("Read more about ", 'familjen-hbg') ?> ' + name + '</a>';
+                //Append html markup for infowindow
+                // jsonPlots[item].info  = '<h3>' + name + '</h3>' + '<p>' + info + '</p>' + '<br><a target="_top" class="btn btn-md btn-primary" href="' + link + '"><?php _e("Read more about ", 'familjen-hbg') ?> ' + name + '</a>';
+                jsonPlots[item].info  = '<h3>' + name + '</h3>' + '<p>' + info + '</p>' + '<br>';
 
-            //     //Create new marker
-            //     marker = new google.maps.Marker({
-            //         position: new google.maps.LatLng(jsonPlots[item].geo.lat,jsonPlots[item].geo.lng),
-            //         name: name,
-            //         map: map
-            //     });
+                //Create new marker
+                marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(jsonPlots[item].geo.lat,jsonPlots[item].geo.lng),
+                    name: name,
+                    map: map
+                });
 
-            //     //Add infowindow trigger
-            //     google.maps.event.addListener(marker, 'click', (function(marker, item) {
-            //       return function() {
-            //           var infoWindow = new google.maps.InfoWindow();
-            //           infoWindow.setContent(jsonPlots[item].info);
-            //           infoWindow.open(map, marker);
-            //       }
-            //     })(marker, item));
+                //Add infowindow trigger
+                google.maps.event.addListener(marker, 'click', (function(marker, item) {
+                  return function() {
+                      var infoWindow = new google.maps.InfoWindow();
+                      infoWindow.setContent(jsonPlots[item].info);
+                      infoWindow.open(map, marker);
+                  }
+                })(marker, item));
 
-            //   }
+              }
             }
           </script>
           <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBcTrRdDFsoCu3bNbfBMU5Me1-9iqChOM8&callback=areaInitMap"></script>
       </div>
     </div>
-
+@endif
