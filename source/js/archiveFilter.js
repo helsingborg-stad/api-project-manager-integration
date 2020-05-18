2afd8a1c298c163dd35ed9b9ca0bc92b4aefb14a
nRamstedt <nikolas.ramstedt@gmail.com>
2020-05-18 11:02:41 +0200

    use Object.keys instead of Object.values (not support in IE11)

--- source/js/archiveFilter.js
+++ source/js/archiveFilter.js
@@ -1,15 +1,16 @@
 export default () => {
-    Object.values(document.querySelectorAll('form.js-submit')).forEach(formElement => {
+    const formElements = document.querySelectorAll('form.js-submit');
+    Object.keys(formElements).map(key => formElements[key]).forEach(formElement => {
         const submitHandler = (e) => {
             formElement.submit();
         };
         
         const selectElements = formElement.querySelectorAll('select');
-        
-        if (selectElements.length <= 0) {
+        if (Object.keys(selectElements).length <= 0) {
             return;
         }
-        selectElements.forEach(selectElement => {
+
+        Object.keys(selectElements).map(key => selectElements[key]).forEach(selectElement => {
             selectElement.addEventListener('change', submitHandler);
         });
 
