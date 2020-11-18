2a19f3a224a9d855c1a2d7ab6786d212c06d8d2d
nRamstedt <nikolas.ramstedt@gmail.com>
2020-11-18 18:11:04 +0100

    fix js error msg

--- source/js/archiveFilter.js
+++ source/js/archiveFilter.js
@@ -1,24 +1,26 @@
 export default () => {
     const formElements = document.querySelectorAll('form.js-submit');
-    Object.keys(formElements).map(key => formElements[key]).forEach(formElement => {
-        const submitHandler = (e) => {
-            Object.values(formElement.elements).forEach(element => {
-                if (element.value === '') {
-                    element.setAttribute('disabled', true);
-                }
-            })
-            formElement.submit();
-        };
-        
-        const selectElements = formElement.querySelectorAll('select');
-        if (Object.keys(selectElements).length <= 0) {
-            return;
-        }
-
-        Object.keys(selectElements).map(key => selectElements[key]).forEach(selectElement => {
-            selectElement.addEventListener('change', submitHandler);
+    if (formElements && formElements.length > 0) {
+        Object.keys(formElements).map(key => formElements[key]).forEach(formElement => {
+            const submitHandler = (e) => {
+                Object.values(formElement.elements).forEach(element => {
+                    if (element.value === '') {
+                        element.setAttribute('disabled', true);
+                    }
+                })
+                formElement.submit();
+            };
+            
+            const selectElements = formElement.querySelectorAll('select');
+            if (Object.keys(selectElements).length <= 0) {
+                return;
+            }
+            
+            Object.keys(selectElements).map(key => selectElements[key]).forEach(selectElement => {
+                selectElement.addEventListener('change', submitHandler);
+            });
+            
         });
-
-    });
+    }
 };
   
