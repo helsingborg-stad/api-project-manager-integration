5b065d106f9835b6489513d3f9bb4d92ab03d14d
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-11-20 08:14:46 +0100

    Register challenge post type

---
+++ source/php/PostTypes/Challange.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ProjectManagerIntegration\PostTypes;
+
+class Challange
+{
+    public $postType = 'challange';
+
+    public function __construct()
+    {
+        add_action('init', array($this, 'registerPostType'), 9);
+    }
+
+    public function registerPostType()
+    {
+        $args = array(
+            'menu_icon'          => 'dashicons-portfolio',
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'query_var'          => true,
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'supports'           => array('title', 'editor', 'thumbnail', 'content', 'excerpt'),
+            'show_in_rest'       => true,
+        );
+
+        $restArgs = array(
+          'exclude_keys' => array('author', 'acf', 'guid', 'link', 'template', 'meta', 'taxonomy', 'menu_order')
+        );
+
+        $postType = new \ProjectManagerIntegration\Helper\PostType(
+            $this->postType,
+            __('Challange', PROJECTMANAGERINTEGRATION_TEXTDOMAIN),
+            __('Challanges', PROJECTMANAGERINTEGRATION_TEXTDOMAIN),
+            $args,
+            array(),
+            $restArgs
+        );
+
+        // Global goals
+        $postType->addTaxonomy(
+            'project'. '_global_goal',
+            __('Global goal', PROJECTMANAGERINTEGRATION_TEXTDOMAIN),
+            __('Global goals', PROJECTMANAGERINTEGRATION_TEXTDOMAIN),
+            array('hierarchical' => true, 'show_ui' => false)
+        );
+
+        $postType->addTaxonomy(
+            'challenge_category',
+            __('Category', PROJECTMANAGERINTEGRATION_TEXTDOMAIN),
+            __('Categories', PROJECTMANAGERINTEGRATION_TEXTDOMAIN),
+            array(
+              'hierarchical' => false,
+              'show_ui' => true,
+            )
+        );
+    }
+}
