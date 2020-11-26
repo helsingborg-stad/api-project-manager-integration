1f262f44faf7f02a97cd2793ea97137809c2147f
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-11-26 18:50:17 +0100

    Set challenge card ratio

--- views/partials/blog/type/post-card-challenge.blade.php
+++ views/partials/blog/type/post-card-challenge.blade.php
@@ -15,10 +15,10 @@
 
 @endphp
 
-<div class="grid-xs-6 {{ $grid_size }}">
+<div class="grid-xs-12 {{ $grid_size }}">
     <a href="{{ $permalink }}" class="box box--project">
         <div class="box__container" data-equal-item>
-            <div class="box__image ratio-1-1" style="background-image:url('{{ municipio_get_thumbnail_source(null,array(500,500), '1:1') }}');">
+            <div class="box__image ratio-12-16" style="background-image:url('{{ municipio_get_thumbnail_source(null,array(636,846), '12:16') }}');">
             </div>
             <div class="box__content">
                 <div class="box__meta">
