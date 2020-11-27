c7880bc1f2cf4f0a3eec53fb6e1e79dcffaceb14
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-11-27 22:20:53 +0100

    set ratio & radius

--- views/partials/blog/type/post-card-challenge.blade.php
+++ views/partials/blog/type/post-card-challenge.blade.php
@@ -18,7 +18,7 @@
 <div class="grid-xs-12 {{ $grid_size }}">
     <a href="{{ $permalink }}" class="box box--project">
         <div class="box__container" data-equal-item>
-            <div class="box__image ratio-12-16" style="background-image:url('{{ municipio_get_thumbnail_source(null,array(636,846), '12:16') }}');">
+            <div class="box__image ratio-12-16 u-radius-8" style="background-image:url('{{ municipio_get_thumbnail_source(null,array(636,846), '12:16') }}');">
             </div>
             <div class="box__content">
                 <div class="box__meta">
