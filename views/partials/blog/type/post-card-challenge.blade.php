b819d82149e22f8b3ba6e3d175f591750d8e0660
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2020-12-01 14:20:19 +0100

    add box featured modifier & implement in challenge cards

--- views/partials/blog/type/post-card-challenge.blade.php
+++ views/partials/blog/type/post-card-challenge.blade.php
@@ -13,18 +13,18 @@
 
 @endphp
 
-<div class="grid-xs-12 {{ $grid_size }}">
-    <a href="{{ $permalink }}" class="box box--project">
+<div class="grid-xs-12 grid-sm-6 grid-lg-3">
+    <a href="{{ $permalink }}" class="box box--project box--feature u-radius-8">
         <div class="box__container" data-equal-item>
             <div class="box__image ratio-12-16 u-radius-8" style="background-image:url('{{ municipio_get_thumbnail_source($post->ID,array(636,846), '12:16') }}');">
             </div>
             <div class="box__content">
-                <div class="box__meta">
-                    @if ($category)
-                        <span class="box__organisation">{{$category}}</span>
-                    @endif
-                </div>
-                <h3 class="box__title">{{ $post->post_title }}</h3>
+                @if ($category)
+                    <div class="box__meta">
+                        <b>{{$category}}</b>
+                    </div>
+                @endif
+                <h3 class="box__title u-mb-0">{{ $post->post_title }}</h3>
             </div>
         </div>
     </a>
