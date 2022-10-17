7b4525f9b5d2c0953dab1261e8821c10ee113cae
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2022-10-17 15:21:05 +0200

    fix: replace h2 tags with h3

--- source/php/UI/ContentPieces.php
+++ source/php/UI/ContentPieces.php
@@ -6,9 +6,17 @@ class ContentPieces
 {
     public static function create(array $items): array
     {
-        return array_filter(
-            $items,
-            fn ($i) => !empty($i['content']) && !empty($i['title'])
+        $replaceH2Tags = fn ($content) => preg_replace('/<h2(.*?)<\/h(.*?)>/', '<h3$1</h3>', $content);
+
+        return array_map(
+            fn ($piece) => [
+                'content' => $replaceH2Tags($piece['content']),
+                'title' => $piece['title']
+            ],
+            array_filter(
+                $items,
+                fn ($i) => !empty($i['content']) && !empty($i['title'])
+            )
         );
     }
 }
