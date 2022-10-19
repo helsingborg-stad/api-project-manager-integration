d05730e7a3e7fbc42fa5ea8f986d2409980f907a
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2022-10-19 10:12:40 +0200

    add: override project items in archive

---
+++ views/partials/post/project-cards.blade.php
@@ -0,0 +1,112 @@
+@if ($posts)
+    <div class="o-grid">
+        @foreach ($posts as $post)
+            @php
+                // Category
+                $category = !empty(get_the_terms($post->ID, 'challenge_category')) ? get_the_terms($post->ID, 'challenge_category')[0]->name : false;
+                
+                $postTags = [];
+                
+                if (!empty(get_the_terms($post->ID, 'project_sector'))) {
+                    $postTags = array_merge($postTags, get_the_terms($post->ID, 'project_sector'));
+                }
+                
+                if (!empty(get_the_terms($post->ID, 'project_technology'))) {
+                    $postTags = array_merge($postTags, get_the_terms($post->ID, 'project_technology'));
+                }
+                
+                if (empty(!$postTags)) {
+                    $postTags = array_reduce(
+                        $postTags,
+                        function ($accumilator, $term) {
+                            if (empty($accumilator)) {
+                                $accumilator = '<span>' . $term->name . '</span>';
+                            } else {
+                                $accumilator .= ' / ' . '<span>' . $term->name . '</span>';
+                            }
+                
+                            return $accumilator;
+                        },
+                        '',
+                    );
+                }
+                
+                $statusBar = \ProjectManagerIntegration\UI\ProjectStatus::create($post->id);
+            @endphp
+
+            <div class="{{ $gridColumnClass }}">
+                @card([
+                    'link' => $post->permalink,
+                    'imageFirst' => true,
+                    'image' => $post->thumbnail,
+                    'heading' => $post->postTitle,
+                    'classList' => ['t-archive-card', 'u-height--100', 'u-display--flex'],
+                    'content' => $post->excerptShort,
+                    'tags' => $post->termsunlinked,
+                    'date' => $post->archiveDate,
+                    'dateBadge' => $post->archiveDateFormat == 'date-badge',
+                    'context' => ['archive', 'archive.list', 'archive.list.card'],
+                    'containerAware' => true,
+                    'hasPlaceholder' => $anyPostHasImage && !isset($post->thumbnail['src'])
+                ])
+                    <div class="c-card__image">
+                        <div class="c-card__image-background"
+                            style="background-image:url('{{ $post->thumbnail['src'] ?? '' }}');">
+                        </div>
+                    </div>
+
+                    <div class="c-card__body">
+                        <div
+                            class="u-display--flex  u-flex-direction--column u-justify-content--space-between u-height--100">
+                            <div>
+                                @typography([
+                                    'element' => 'span',
+                                    'variant' => 'h6',
+                                    'classList' => ['u-margin__bottom--2']
+                                ])
+                                    {!! $category !!}
+                                @endtypography
+                                @typography([
+                                    'element' => 'h3',
+                                    'variant' => 'h2',
+                                    'classList' => ['c-card__heading', 'u-margin__y--05']
+                                ])
+                                    {!! $post->postTitle !!}
+                                @endtypography
+
+                                @if (!empty($postTags))
+                                    <div>
+                                        {!! $postTags !!}
+                                    </div>
+                                @endif
+                            </div>
+                            <div>
+                                @if (!empty($statusBar) && $statusBar['value'] > -1 && $statusBar['label'])
+                                    <div class="statusbar u-margin__top--2">
+                                        <div class="statusbar__header u-mb-1 explain">
+                                            <b class="statusbar__title">{{ $statusBar['label'] }}</b>
+
+                                            @if (!empty($statusBar['explainer']))
+                                                <span class="statusbar__explainer">
+                                                    <span data-tooltip="{{ $statusBar['explainer'] }}" data-tooltip-bottom>
+                                                        <i class="pricon pricon-info-o"></i>
+                                                    </span>
+                                                </span>
+                                            @endif
+                                        </div>
+                                        <div class="statusbar__content">
+                                            <div class="c-progressbar">
+                                                <div class="c-progressbar__value {{ $statusBar['isCancelled'] ? 'is-disabled' : '' }}"
+                                                    style="width: {{ $statusBar['value'] }}%;"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                @endcard
+            </div>
+        @endforeach
+    </div>
+@endif
