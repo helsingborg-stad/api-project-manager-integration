4124e1224f7bd09d5517881d5b3598e8047d7897
NiclasNorin <niclas.norin@frontwalker.se>
2022-12-13 17:05:24 +0100

    Global goals and Related posts

---
+++ views/partials/challenge/related-posts.blade.php
@@ -0,0 +1,43 @@
+@group([
+    'justifyContent' => 'space-between',
+    'classList' => ['challenge__related', 'u-margin__bottom--3']
+])
+    @typography([
+        'variant' => 'h2',
+        'element' => 'h3',
+        'classList' => ['challenge__related-heading'],
+    ])
+        {{$lang['more']}} 
+    @endtypography
+    @link([
+        'href' => $archive,
+        'classList' => ['challenge__related-link'],
+    ])
+        @group([
+            'alignItems' => 'center',
+        ])
+            {{$lang['showAll']}}
+            @icon([
+                'icon' => 'arrow_forward',
+                'classList' => ['challenge__related-icon'],
+                'size' => 'lg'
+                ])
+            @endicon
+        @endgroup
+    @endlink
+@endgroup
+
+<div class="o-grid">
+    @foreach($relatedPosts as $item)
+    <div class="o-grid-3@md">
+        @block([
+            'heading' => $item->post_title,
+            'meta' => $item->category,
+            'ratio' => '12:16',
+            'image' => ['src' => $item->thumbnail, 'alt' => $item->post_title],
+            'link' => $item->url
+                ])
+        @endblock
+    </div>
+    @endforeach
+</div> 
