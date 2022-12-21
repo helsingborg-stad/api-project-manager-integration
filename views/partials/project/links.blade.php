631589329b8099125e2e896ebbb131615daac840
NiclasNorin <niclas.norin@frontwalker.se>
2022-12-21 11:47:48 +0100

    Github change requests fixed

--- views/partials/project/links.blade.php
+++ views/partials/project/links.blade.php
@@ -1,11 +1,16 @@
 <div class="o-grid-12">
     @card
         <div class="c-card__body u-pt-1">
-            <h4>{{$lang['linksLabel']}}</h4>
+            @typography([
+                'element' => 'h2',
+                'variant' => 'h4',
+            ])
+                {{$lang['linksLabel']}}
+            @endtypography
             <ul>
                 @foreach ($project['links'] as $link)
                     <li>
-                        <a target="_blank" href="{{ $link['url'] }}">{{ $link['title'] }}</a>
+                        <a rel="noopener" target="_blank" href="{{ $link['url'] }}">{{ $link['title'] }}</a>
                     </li>
                 @endforeach
             </ul>
