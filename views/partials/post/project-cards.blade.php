5e941268d1158d082686ba48050a18481848a177
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-01-05 12:54:34 +0100

    fix: undefined errors

--- views/partials/post/project-cards.blade.php
+++ views/partials/post/project-cards.blade.php
@@ -43,8 +43,6 @@
                     'classList' => ['t-archive-card', 'u-height--100', 'u-display--flex'],
                     'content' => $post->excerptShort,
                     'tags' => $post->termsunlinked,
-                    'date' => $post->archiveDate,
-                    'dateBadge' => $post->archiveDateFormat == 'date-badge',
                     'context' => ['archive', 'archive.list', 'archive.list.card'],
                     'containerAware' => true,
                     'hasPlaceholder' => $anyPostHasImage && !isset($post->thumbnail['src'])
