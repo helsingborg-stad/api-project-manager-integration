73b4e785cd4964b21a4be3a5e96935e2920a2c98
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2023-01-10 17:00:24 +0100

    source/php/UI/RelatedPosts: getPosts using helper & add optional param $wpQueryArgs

--- source/php/UI/RelatedPosts.php
+++ source/php/UI/RelatedPosts.php
@@ -2,25 +2,19 @@
 
 namespace ProjectManagerIntegration\UI;
 
-use \Municipio\Helper\FormatObject;
-use \Municipio\Helper\Post;
+use ProjectManagerIntegration\Helper\Municipio;
 
 class RelatedPosts
 {
     public static function create(
         int $postId = 0,
         string $postType = '',
-        string $relatedPostTitle = ''
+        string $relatedPostTitle = '',
+        array $wpQueryArgs = []
     ): array {
         $post = $postId > 0 ? $postId : get_queried_object_id();
-        $type = !empty($postType) ? $postType : get_post_type();
 
-        $posts = get_posts([
-            'post_type' => $type,
-            'posts_per_page' => 4,
-            'exclude' => array($post),
-            'orderby' => 'rand'
-        ]) ?? [];
+        $type = !empty($postType) ? $postType : get_post_type();
 
         $postTypeObject = get_post_type_object($type);
 
@@ -29,20 +23,16 @@ class RelatedPosts
             PROJECTMANAGERINTEGRATION_TEXTDOMAIN
         ) . ' ' . $postTypeObject->labels->all_items;
 
-        $mapPostDataForMunicipio = fn ($p) => (object) array_merge(
-            (array) FormatObject::camelCase($p),
-            [
-                'permalink' => get_permalink($p->ID),
-                'thumbnail' => Post::getFeaturedImage($p->ID, [800, 800]),
-            ]
-        );
+        $posts = Municipio::getPosts(array_merge([
+            'post_type' => $type,
+            'posts_per_page' => 4,
+            'exclude' => array($post),
+            'orderby' => 'rand',
+        ], $wpQueryArgs)) ?? [];
 
         return count($posts) > 0 ? [
             'title'         => $title,
-            'posts'         => array_map(
-                $mapPostDataForMunicipio,
-                $posts
-            )
+            'posts'         => $posts
         ] : [];
     }
 }
